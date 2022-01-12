e11f597dc910561cc74f627accd879eb7cdfe79c
tniemeye19 <timothy.niemeyer19@gmail.com>
2022-01-11 19:54:48 -0500

    implement apollo-server-express. create schemas folder so graphql can be initialized and used

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const path = require('path');
+
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  // start the apollo server
+  await server.start();
+  // integrate our apollo server with the express application as middleware
+  server.applyMiddleware({ app });
+  // log where we can go to test out GL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +37,19 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}!`);
+  });
 });
